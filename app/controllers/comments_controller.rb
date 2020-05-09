c449d5981e3394aca26c0328c6a659e63f75b05a
jokale <kalemajoanna@gmail.com>
2020-05-09 14:23:05 +0100

    Create controller action methods and before action filter in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,46 @@
 class CommentsController < ApplicationController
+
+    before_action: :set_threadd
+
+    def index
+        @comments = @threadd.comment.all 
+        render json: @comments
+    end 
+
+    def create
+        @comment = Comment.new(comment_params) 
+        if @comment.save 
+        render json: @comment 
+        else  
+            render json: {error: 'Error creating a new thread please try again'}
+        end 
+    end 
+
+    def show
+        @comment = Comment.find(params[:id])
+        render json: @comment, include: [:comment]
+    end 
+
+   
+    def update
+        # @comment = Comment.find(params[:id])
+        # @comment.update(comment_params)
+        # render json: @comment
+    end 
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.delete 
+        render json: {commentId: @comment.id}
+    end 
+
+    def set_threadd
+        @threadd = Threadd.find(params[:threadd_id])
+    end 
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:threadd_id, :username, :date, :comment_description)
+    end 
 end
--- app/controllers/threadds_controller.rb
+++ app/controllers/threadds_controller.rb
@@ -1,2 +1,44 @@
 class ThreaddsController < ApplicationController
+
+    def index
+        @threadds = Threadd.all 
+        render json: @threadds, include: [:comments]
+    end 
+
+    def create
+        @threadd = Threadd.new(threadd_params) 
+        if @threadd.save 
+        render json: @threadd 
+        else  
+            render json: {error: 'Error creating a new thread please try again'}
+        end 
+    end 
+
+    def show
+        @threadd = Threadd.find(params[:id])
+        render json: @threadd, include: [:comment]
+    end 
+
+   
+    def update
+        @threadd = Threadd.find(params[:id])
+        @threadd.update(threadd_params)
+        render json: @threadd
+    end 
+
+    def destroy
+        @threadd = Threadd.find(params[:id])
+        @threadd.delete 
+        render json: {threaddId: @threadd.id}
+    end 
+
+
+    private
+
+    def threadd_params
+        params.require(:threadd).permit(:username, :date, :thread_title, :thread_body, comment_attributes:[:threadd_id, :username, :date, :comment_description])
+    end 
+
+
+
 end
